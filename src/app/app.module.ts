f7e6c4626d1087174996a4c30d00305f982460b2
Serah wakaba <serahwakaba9@gmail.com>
2021-05-17 09:22:43 +0300

    imports commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { QuoteComponent } from './quote/quote.component';
-// import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-// import { FormComponent } from './form/form.component';
-// import {FormsModule} from '@angular/forms'
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { FormComponent } from './form/form.component';
+import {FormsModule} from '@angular/forms'
 
 
 @NgModule({
